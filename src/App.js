c6b8d1d2f7c6b3de0d8e1ab3a96b9e5e0b8f1936
jaideepghosh <er.jaideepghosh@gmail.com>
2021-08-27 13:33:00 +0530

    feat:Added dynamic QR generator.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, {useEffect, useState} from "react";
+import QRCode from "qrcode.react";
 import './App.css';
 
 function App() {
-  const [activeUrl, setActiveUrl] = useState("");
+  const [activeUrl, setActiveUrl] = useState(window.location.toString());
   useEffect(() => {
-    window.chrome.tabs.getSelected(null, (tab)=> {
-      setActiveUrl(tab.url)
-    });
+    if(window.chrome && window.chrome.tabs && window.chrome.tabs.getSelected){
+      window.chrome.tabs.getSelected(null, (tab)=> {
+        setActiveUrl(tab.url)
+      });
+    }
   });
   return (
     <div className="app">
-        <img src="https://www.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/core_market_full/generator/dist/generator/assets/images/websiteQRCode_noFrame.png" alt="logo" className="qr" />
+        <QRCode value={activeUrl} className="qr" />
         <p className="message">Scan the above QR from your phone to open this active link in your mobile device.</p>
-        {
-          activeUrl
-        }
     </div>
   );
 }
